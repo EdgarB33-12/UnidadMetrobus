15e78831b60f97705e6cf2c26649c13f29665b0a
edgar <edgar@localhost>
2020-07-01 20:15:48 -0500

    Primer Comentario

---
+++ src/main/java/com/metrobus/UnidadMetrobusApplication.java
@@ -0,0 +1,13 @@
+package com.metrobus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidadMetrobusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnidadMetrobusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metrobus/UnidadMetrobusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metrobus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidadMetrobusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
